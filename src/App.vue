6fe741b7f3277c2173c7cc76ba6aa78ba1bfed5b
ArditVezaj <arditvezaj1@gmail.com>
2023-02-12 20:37:40 +0100

    made it stylish

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,79 @@
   <section>
     <h1>Money Converter</h1>
     <h3>Easiest and simplest Money Converter in Internet</h3>
-    <div>
-      <label for="from"></label>
-      <select name="from" id="from">
-        <option value="Franc">CHF - Swiss Franc</option>
-        <option value="Euro">EUR - Euro</option>
-        <option value="Dollar">USD - Dollar</option>
-        <option value="Pound">GBP - British Pound</option>
-      </select>
+    <div class="body">
+      <div class="main">
+        <label for="from">From</label>
+        <select name="from" id="from">
+          <option value="franc">CHF - Swiss Franc</option>
+          <option value="euro">EUR - Euro</option>
+          <option value="dollar">USD - Dollar</option>
+          <option value="pound">GBP - British Pound</option>
+        </select>
+      </div>
+
+      <button>Change sides</button>
+
+      <div class="main">
+        <label for="to">To</label>
+        <select name="to" id="to">
+          <option value="franc">CHF - Swiss Franc</option>
+          <option value="euro">EUR - Euro</option>
+          <option value="dollar">USD - Dollar</option>
+          <option value="pound">GBP - British Pound</option>
+        </select>
+      </div>
     </div>
 
-    <button>Change sides</button>
-
-    <div>
-      <select name="to" id="to">
-        <option value="Franc">CHF - Swiss Franc</option>
-        <option value="Euro">EUR - Euro</option>
-        <option value="Dollar">USD - Dollar</option>
-        <option value="Pound">GBP - British Pound</option>
-      </select>
+    <div class="body">
+      <div class="main">
+        <label for="amount">Amount</label>
+        <input type="number" name="amount" />
+      </div>
+      <div class="main">
+        <label for="result">Result</label>
+        <input type="number" name="result" disabled/>
+      </div>
     </div>
-
-    <label for="amount">Amount</label>
-    <input type="number" name="amount" />
-
-    <h2>Result: (result)</h2>
   </section>
 </template>
+
+<style scoped>
+section {
+  margin: 4rem 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(108, 148, 242);
+  border-radius: 8px;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+.body {
+  display: flex;
+  align-items: center;
+}
+
+.main {
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+select,
+input {
+  margin-bottom: 1rem;
+  width: 12rem;
+  height: 3rem;
+}
+
+select,
+input,
+label {
+  font-size: 1.2rem;
+}
+</style>
